86ab7eca4ee6433e8c01ef0e4b933c70871d1fa8
tuanvu0801junda <tuanvutruonglacnhatsinh0801@gmail.com>
2022-03-13 22:10:16 +0700

    complete sign in

--- react/src/layouts/authentication/sign-in/index.js
+++ react/src/layouts/authentication/sign-in/index.js
@@ -43,9 +43,40 @@ import bgImage from "assets/images/bg-sign-in-basic.jpeg";
 
 function Basic() {
   const [rememberMe, setRememberMe] = useState(false);
-
   const handleSetRememberMe = () => setRememberMe(!rememberMe);
 
+  const [state, setState] = useState({
+    "username": "",
+    "password": ""
+  })
+
+  const handleInput = (e) => {
+    e.persist();
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const signIn = async () => {
+    if (state.username === ""){
+      console.log("Username is empty!");
+    } else if (state.password === ""){
+      console.log("Password is empty");
+    } else{
+      const data = {
+        "username" : state.username,
+        "password" : state.password
+      }
+
+      console.log(state)
+
+      // const response = axios.post('http://127.0.0.1:8000/todo/signIn', data)
+      // if (response.data.status === "OK"){
+      //   console.log(response.data)
+      // } else if (response.data.status === "ERROR"){
+      //   console.log(response.data)
+      //   console.log("ERROR happened!")
+      // }
+    }
+  }
   return (
     <BasicLayout image={bgImage}>
       <Card>
@@ -63,32 +94,55 @@ function Basic() {
           <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
             Sign in
           </MDTypography>
+
           <Grid container spacing={3} justifyContent="center" sx={{ mt: 1, mb: 2 }}>
+
             <Grid item xs={2}>
               <MDTypography component={MuiLink} href="#" variant="body1" color="white">
                 <FacebookIcon color="inherit" />
               </MDTypography>
             </Grid>
+
             <Grid item xs={2}>
               <MDTypography component={MuiLink} href="#" variant="body1" color="white">
                 <GitHubIcon color="inherit" />
               </MDTypography>
             </Grid>
+
             <Grid item xs={2}>
               <MDTypography component={MuiLink} href="#" variant="body1" color="white">
                 <GoogleIcon color="inherit" />
               </MDTypography>
             </Grid>
+
           </Grid>
         </MDBox>
+
         <MDBox pt={4} pb={3} px={3}>
           <MDBox component="form" role="form">
+
             <MDBox mb={2}>
-              <MDInput type="email" label="Email" fullWidth />
+              <MDInput
+                name="username"
+                type="username"
+                label="UserName"
+                fullWidth
+                value={state.username}
+                onChange={handleInput}
+              />
             </MDBox>
+
             <MDBox mb={2}>
-              <MDInput type="password" label="Password" fullWidth />
+              <MDInput
+                name="password"
+                type="password"
+                label="Password"
+                fullWidth
+                value={state.password}
+                onChange={handleInput}
+              />
             </MDBox>
+
             <MDBox display="flex" alignItems="center" ml={-1}>
               <Switch checked={rememberMe} onChange={handleSetRememberMe} />
               <MDTypography
@@ -101,11 +155,18 @@ function Basic() {
                 &nbsp;&nbsp;Remember me
               </MDTypography>
             </MDBox>
+
             <MDBox mt={4} mb={1}>
-              <MDButton variant="gradient" color="info" fullWidth>
+              <MDButton
+                variant="gradient"
+                color="info"
+                fullWidth
+                onClick={signIn}
+              >
                 sign in
               </MDButton>
             </MDBox>
+
             <MDBox mt={3} mb={1} textAlign="center">
               <MDTypography variant="button" color="text">
                 Don&apos;t have an account?{" "}
@@ -121,6 +182,7 @@ function Basic() {
                 </MDTypography>
               </MDTypography>
             </MDBox>
+
           </MDBox>
         </MDBox>
       </Card>
